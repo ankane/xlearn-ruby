2e4bc53c7d45f510c5949a3d1462cba93cc59e29
Andrew Kane <andrew@chartkick.com>
2019-10-12 12:57:58 -0700

    Fix for Windows

--- ext/xlearn/extconf.rb
+++ ext/xlearn/extconf.rb
@@ -9,11 +9,15 @@ end
 
 dir = File.expand_path("../../vendor/xlearn", __dir__)
 
+arch = RbConfig::CONFIG["arch"]
+puts "Arch: #{arch}"
+make_prefix = arch =~ /mingw/ ? "ridk exec " : ""
+
 build_dir = "#{dir}/build"
 Dir.mkdir(build_dir)
 Dir.chdir(build_dir) do
   run "cmake .."
-  run "make -j4"
+  run "#{make_prefix}make -j4"
 end
 
 create_makefile("xlearn")
