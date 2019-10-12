594f01ca760aca84c3175054b9f5f70ac2758013
Andrew Kane <andrew@chartkick.com>
2019-10-12 13:49:26 -0700

    Speed up tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,9 +2,7 @@ require "bundler/setup"
 Bundler.require(:default)
 require "minitest/autorun"
 require "minitest/pride"
-require "daru"
 require "matrix"
-require "numo/narray"
 
 class Minitest::Test
   def assert_elements_in_delta(expected, actual, delta = 0.001)
