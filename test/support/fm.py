ee840b9abed8d4c8700972dc97a5d844593e2a10
Andrew Kane <andrew@chartkick.com>
2020-08-19 04:17:28 -0700

    Test csv and text format [skip ci]

--- test/support/fm.py
+++ test/support/fm.py
@@ -1,7 +1,7 @@
 import xlearn as xl
 import pandas as pd
 
-df = pd.read_csv('test/support/data.csv', sep=' ', names=['y', 'x0', 'x1', 'x2', 'x3'])
+df = pd.read_csv('test/support/data.txt', sep=' ', names=['y', 'x0', 'x1', 'x2', 'x3'])
 
 X = df.drop(columns=['y'])
 y = df['y']
@@ -11,5 +11,9 @@ model.fit(X, y)
 print('predict', model.predict(X)[0:6].tolist())
 
 model = xl.FMModel(task='reg', nthread=1, opt='adagrad')
+model.fit('test/support/data.txt')
+print('predict txt', model.predict('test/support/data.txt')[0:6].tolist())
+
+model = xl.FMModel(task='reg', nthread=1, opt='adagrad')
 model.fit('test/support/data.csv')
-print('predict', model.predict('test/support/data.csv')[0:6].tolist())
+print('predict csv', model.predict('test/support/data.csv')[0:6].tolist())
