8b8ab987ed878c8954a6f4c92d712abea4ced435
Andrew Kane <andrew@chartkick.com>
2020-08-19 03:59:02 -0700

    Improved test scripts [skip ci]

--- test/support/fm.py
+++ test/support/fm.py
@@ -6,10 +6,10 @@ df = pd.read_csv('test/support/data.csv', sep=' ', names=['y', 'x0', 'x1', 'x2',
 X = df.drop(columns=['y'])
 y = df['y']
 
-model = xl.LRModel(task='reg', nthread=1, opt='adagrad')
+model = xl.FMModel(task='reg', nthread=1, opt='adagrad')
 model.fit(X, y)
 print('predict', model.predict(X)[0:6].tolist())
 
-model = xl.LRModel(task='reg', nthread=1, opt='adagrad')
+model = xl.FMModel(task='reg', nthread=1, opt='adagrad')
 model.fit('test/support/data.csv')
 print('predict', model.predict('test/support/data.csv')[0:6].tolist())
